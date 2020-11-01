9db3c281b677acd13086269ddf11a7e8bd0bfcf1
Chrizthian <1521509@utp.edu.pe>
2020-11-01 11:13:43 -0500

    Error
    Cambio del error

--- PortalDenuncia/Controllers/ExportarDatosController.cs
+++ PortalDenuncia/Controllers/ExportarDatosController.cs
@@ -60,7 +60,7 @@ namespace PortalDenuncia.Controllers
 
             excelshe.Cells["A:AZ"].AutoFitColumns();
             Response.Clear();
-            Response.ContentType = "application/vnd.openxmlformats.officedocument.spreadsheetml.sheet";
+            Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
             Response.AddHeader("content-disposition", "attachment: filename=" + "ExcelReport.xlsx");
             Response.BinaryWrite(excelpack.GetAsByteArray());
             Response.End();
