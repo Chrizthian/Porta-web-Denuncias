3797ce2bcc96e881e4da0074f446e9cef9e895ee
Chrizthian <1521509@utp.edu.pe>
2020-11-01 10:08:46 -0500

    Nuevo controlador
    Controlador archivos

---
+++ PortalDenuncia/Controllers/ExportarDatosController.cs
@@ -0,0 +1,19 @@
+﻿using PortalDenuncia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortalDenuncia.Controllers
+{
+    public class ExportarDatosController : Controller
+    {
+        private DBDenunciaEntities db = new DBDenunciaEntities();
+        // GET: ExportarDatos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
