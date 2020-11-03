521857e6bcf621b7ae3a0b3e675d93c5e23f817f
Chrizthian <1521509@utp.edu.pe>
2020-11-02 23:25:41 -0500

    Delegado graf
    Preparar consultas de graf

--- PortalDenuncia/Controllers/DelegadoController.cs
+++ PortalDenuncia/Controllers/DelegadoController.cs
@@ -32,5 +32,28 @@ namespace PortalDenuncia.Controllers
 
             return View(tBDENUNCIAS.Where(u => u.iddelegado == tbdelegado.iddelegado).ToList());
         }
+
+        public ActionResult DelegadoDraf1()
+        {
+            DenunciaCount denuncian = new DenunciaCount();
+            denuncian.alerta = db.TBDENUNCIAS.Where(x => x.idtipo == 1).Count();
+            denuncian.denuncia = db.TBDENUNCIAS.Where(x => x.idtipo == 2).Count();
+
+            return Json(denuncian, JsonRequestBehavior.AllowGet);
+        }
+
+        public void ExportarDelegado1()
+        {
+            var tBDENUNCIAS = db.TBDENUNCIAS
+                .Include(t => t.TBCOMISARIA)
+                .Include(t => t.TBDISTRITO)
+                .Include(t => t.TBESTADO)
+                .Include(t => t.TBPOLICIA)
+                .Include(t => t.TBTIPODENUNCIA)
+                .Include(t => t.TBVERAZIDAD);
+            
+            //Colocar la lista
+            //Realizar transformacion
+        }
     }
 }
