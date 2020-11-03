133e9dd3c83017241bf61761423a5d7b5f29064a
Chrizthian <1521509@utp.edu.pe>
2020-11-02 21:10:51 -0500

    Reporte 3
    Adicion de otro reporte

---
+++ PortalDenuncia/Controllers/DelegadoController.cs
@@ -0,0 +1,30 @@
+﻿using PortalDenuncia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortalDenuncia.Controllers
+{
+    public class DelegadoController : Controller
+    {
+        // GET: Delegado
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListaDenuncia()
+        {
+            TBDELEGADO tbusuario = (TBDELEGADO)Session["delegado"];
+
+            var tBDENUNCIAS = db.TBDENUNCIAS
+                .Include(t => t.TBCOMISARIA)
+                .Include(t => t.TBDISTRITO)
+                .Include(t => t.TBESTADO)
+                .Include(t => t.TBTIPODENUNCIA);
+            return View(tBDENUNCIAS.Where(u => u.idusuario == tbusuario.idusuario).ToList());
+        }
+    }
+}
---
+++ PortalDenuncia/Models/DenunciaRep.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortalDenuncia.Models
+{
+    public class DenunciaRep
+    {
+        int codigo { get; set; }
+        String detalle { get; set; }
+        String direccion { get; set; }
+        String tipod { get; set; }
+        String distrrito { get; set; }
+        String estado { get; set; }
+        String veracidad { get; set; }
+
+        public DenunciaRep(int codigo, String detalle, String direccion, String tipod, String distrrito, 
+            String estado, String veracidad)
+        {
+            this.codigo = codigo;
+            this.detalle = detalle;
+            this.direccion = direccion;
+            this.tipod = tipod;
+            this.distrrito = distrrito;
+            this.estado = estado;
+            this.veracidad = veracidad;
+        }
+    }
+}
